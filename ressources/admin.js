48158fdb437dd0d846523e55e4102f3d8dd6862b
herewithme <amaury@beapi.fr>
2011-03-02 11:29:55 +0000

    keep the correct tab open... after saving :)

--- ressources/admin.js
+++ ressources/admin.js
@@ -1,23 +1,36 @@
 jQuery(document).ready(function() {
 	toggleMode();
-	
+
 	jQuery('input[name="query_mode"]').change(function() {
 		toggleMode();
 	});
-	
-	jQuery(function() {
-		jQuery( "#accordion" ).accordion({ autoHeight: false });
+
+	var userpanel = jQuery("#accordion");
+	var index = jQuery.cookie("accordion");
+	var active;
+	if (index !== undefined) {
+		active = userpanel.find("h3:eq(" + index + ")");
+	}
+	userpanel.accordion({
+		autoHeight: false,
+		active: active,
+		change: function(event, ui) {
+			var index = jQuery(this).find("h3").index(ui.newHeader[0]);
+			jQuery.cookie("accordion", index);
+		}
 	});
-	
-	jQuery( "#accordion h3 a" ).click(function(event) {
+
+	jQuery("#accordion h3 a").click(function(event) {
 		event.preventDefault();
-		event.parent().click();
+		jQuery(event).parent().click();
 	});
 });
 
+
+
 function toggleMode() {
 	jQuery('#simple-mode-query, #advanced-mode-query').removeClass('hide-if-no-js');
-	if ( jQuery('input[name="query_mode"]:checked').val() == 'advanced' ) {
+	if (jQuery('input[name="query_mode"]:checked').val() == 'advanced') {
 		jQuery('#simple-mode-query').hide();
 		jQuery('#advanced-mode-query').show();
 	} else {
